8e6507b8c0a12282242f5fb3626967a4b9aeac74
Thirupathi Addisherla <thirupathiaddisherla@gmail.com>
2024-02-25 22:28:02 +0530

    creating react components

---
+++ src/components/Profile.js
@@ -0,0 +1,8 @@
+export function Profile() {
+    return (
+        <img
+            src="https://cdn.britannica.com/09/75509-131-93B65AAF/Albert-Einstein.jpg?w=200&h=200&c=crop"
+            alt="Albert Einstein"
+        />
+    );
+}
